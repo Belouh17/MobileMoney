93fed3ea9ceae5fc9746f9b62dfa8da327c6e0eb
zenoxys-k <kiadyramerison@gmail.com>
2026-07-20 16:23:19 +0300

    Implement OperateurFilter for authentication and session management; add home view for operator dashboard

---
+++ app/Views/operateur/home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+Bienvenue admin
+</h1>
+
+
+
+
+
+<a href="/operateur/comptes">
+comptes
+</a>
+
+
+<a href="/operateur/prefixes">Prefixes</a>
+
+
+<a href="/operateur/baremes/1">Baremes</a>
+
+</body>
+</html>
