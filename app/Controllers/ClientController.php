73f5528143154d61850e178e84e08b80f3ccbb31
Fabrice <fabricetokiniaina17@gmail.com>
2026-07-20 09:30:43 +0300

    client_page et login

---
+++ app/Controllers/ClientController.php
@@ -0,0 +1,14 @@
+<?php 
+
+class ClientController extends BaseController
+{
+    public function index()
+    {
+        return view('client/dashbord');
+    }
+
+    public function register()
+    {
+        return view('client/register');
+    }
+}
