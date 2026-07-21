d550f12749b1c1940f67ebef8afcb58dda7fb017
zenoxys-k <kiadyramerison@gmail.com>
2026-07-21 09:48:53 +0300

    Remove login form from home.php and update navigation links for operateur and client

--- app/Views/home.php
+++ app/Views/home.php
@@ -6,16 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Connexion Mobile Money Operateur</h1>
-    <form action="/operateur/auth" method="post">
-        <label for="username">Nom d'utilisateur:</label>
-        <input type="text" id="username" name="username" required><br><br>
-
-        <label for="password">Mot de passe:</label>
-        <input type="password" id="password" name="password" required><br><br>
-
-        <input type="submit" value="Se connecter">
-    </form>
-    connexion client  <a href="/client/login">client </a>
+    <a href="/operateur/login">operateur </a>
+    <a href="/client/login">client </a>
 </body>
 </html>
